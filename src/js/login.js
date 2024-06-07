1956238fea439663c9e7ae1844d814d555cef2b9
Mohamed Smaili <mohamedafifsmaili@gmail.com>
2024-06-06 21:07:23 -0300

    validacao do login

---
+++ src/js/login.js
@@ -0,0 +1,27 @@
+function validarLogin() {
+    var email = document.getElementById('emailLogin').value.trim();
+    var senha = document.getElementById('senhaLogin').value.trim();
+
+    if (email === '' || senha === '') {
+        alert('Por favor, preencha todos os campos.');
+        return false;
+    }
+
+    var usuarioArmazenado = localStorage.getItem('usuario');
+    if (!usuarioArmazenado) {
+        alert('Usuário não cadastrado. Por favor, realize o cadastro.');
+        return false;
+    }
+
+    var usuario = JSON.parse(usuarioArmazenado);
+    if (usuario.email !== email || usuario.senha !== senha) {
+        alert('Credenciais inválidas. Por favor, verifique seu e-mail e senha.');
+        return false;
+    }
+
+    alert('Login realizado com sucesso!\nBem-vindo, ' + usuario.nome + '!');
+
+    window.location.href = 'index.html';
+
+    return true;
+}
